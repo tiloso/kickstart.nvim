45da45ff6cc905916b72b9092764344a18d79070
tiloso <tilo.soenke@gmail.com>
2025-04-03 18:11:12 +0200

    Remove oil.nvim

--- init.lua
+++ init.lua
@@ -985,29 +985,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('oil').setup {
-        columns = { 'icon' },
-        keymaps = {
-          ['<C-h>'] = false,
-          ['<M-h>'] = 'actions.select_split',
-        },
-        view_options = {
-          show_hidden = true,
-        },
-      }
-
-      -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-
-      -- Open parent directory in floating window
-      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
-    end,
-  },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
