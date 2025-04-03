412eb6618e6367ea29bfe1bff5577f87a361ef7e
tiloso <tilo.soenke@gmail.com>
2025-04-03 18:08:29 +0200

    Use tsserver instead ts_ls

--- init.lua
+++ init.lua
@@ -710,7 +710,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
+        tsserver = {},
         terraformls = {},
 
         lua_ls = {
@@ -1141,7 +1142,7 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        -- additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
